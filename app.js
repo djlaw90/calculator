3525f10a015157b5239cc9bf17a061b0a08604b7
djlaw90 <djlaw90@gmail.com>
2023-10-10 15:35:14 -0500

    basic functionality down. next step: handle long decimals

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const calcButtons = document.querySelectorAll('.calc-buttons');
 
-const display = document.querySelector('.display-output');
+const mainDisplay = document.querySelector('.display-output');
 const displayTop = document.querySelector('.display-output-top');
 
 const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
 
 //Operator functions
 const add = (a,b) =>  a + b;
@@ -11,68 +12,82 @@ const subtract = (a,b) =>  a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
-let number1 = 0;
-let number2 = 0;
-let operation = '';
-let operatorSign = '';
+let operand1 = 0;
+let operand2 = 0;
+let operatorSign = null;
+let shouldResetDisplay = false;
 
-const operate = (operator, number1, number2) => {
+const operate = (number1, number2) => {
     let result;
-    if(operator === "add") {
+    if(operatorSign === "+") {
         result = add(number1, number2);
-    } else if (operator === "subtract") {
+    } else if (operatorSign === "-") {
         result = subtract(number1, number2);
-    } else if (operator === "multiply") {
+    } else if (operatorSign === "×") {
         result = multiply(number1, number2);
     } else {
         result = divide(number1, number2);
     }
+    displayTop.textContent = `${operand1} ${operatorSign} ${operand2} =`
+    mainDisplay.textContent = result;
 }
 
-const getNumberVal = buttonClicked => {
-    number1 = +buttonClicked.innerHTML;
-    displayVal(number1);
+const appendNum = numClicked => {
+    if(mainDisplay.textContent === '0' || shouldResetDisplay) {
+        resetDisplay();
+    }
+    mainDisplay.textContent += numClicked.textContent;
 }
 
-const getFuncVal = buttonClicked => {
-    operation = buttonClicked.getAttribute('value');
+
+const resetDisplay = () => {
+    shouldResetDisplay = false;
+    mainDisplay.textContent = '';
+}
+
+const setOperation = currentOperation => {
+    newInput = true;
+    let operation = currentOperation.getAttribute('value');
     if(operation === "add") {
         operatorSign = "+";
     } else if(operation === "subtract") {
-        operatorSign = "-"
+        operatorSign = "-";
     } else if(operation === "multiply") {
-        operatorSign = "×"
+        operatorSign = "×";
     } else if(operation === "divide") {
-        operatorSign = "÷"
+        operatorSign = "÷";
     }
-    displayFunc(operatorSign);
+    operand1 = +mainDisplay.textContent;
+    displayTop.textContent = `${operand1} ${operatorSign}`;
+    shouldResetDisplay = true;
 }
 
-const displayFunc = operator => {
-    displayTop.innerHTML = number1;
-    displayTop.innerHTML += operator;
+const evaluate = () => {
+    operand2 = +mainDisplay.textContent;
+    operate(operand1, operand2);
 }
-
-const displayVal = value => {
-    display.innerHTML = value;
-}
-
 
 const clear = () => {
-    display.innerHTML = '';
+    mainDisplay.innerHTML = '';
     displayTop.innerHTML = '';
+    number1 = 0;
+    number2 = 0;
+    operation = '';
+    operatorSign = '';
 } 
 
-
 clearButton.addEventListener('click', clear);
+
+equalButton.addEventListener('click', evaluate);
 
 calcButtons.forEach(button => {
     button.addEventListener("click", (e) => {
         if(e.target.classList.contains('number')) {
-            getNumberVal(e.target);
+            appendNum(e.target);
         } else if(e.target.classList.contains('function')) {
-            getFuncVal(e.target);
-            displayFunc(operatorSign);
+            setOperation(e.target);
         }
     });
   }); 
+
+  //init();
